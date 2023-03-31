3900072dff797e0a774f1d16caf9c9a9d5eea0b3
Abuka-Victor <abukavictoro@gmail.com>
2023-03-31 19:02:11 +0100

    Add add to cart functionality

--- src/Components/Cart-DropDown/cart-dropdown.component.jsx
+++ src/Components/Cart-DropDown/cart-dropdown.component.jsx
@@ -1,12 +1,21 @@
-import React from 'react';
+import { useContext } from 'react';
+import { cartOpenContext } from '../../Contexts/CartOpen.context.js';
 
 import Button from '../Button/Button.component.js';
+import CartItem from '../Cart-Item/cart-item.component.jsx';
 import './cart-dropdown.styles.scss';
 
 const CartDropdown = () => {
+  const { cartItems } = useContext(cartOpenContext);
+
   return (
     <div className="cart-dropdown-container">
       <div className="cart-items">
+        <div>
+          {cartItems.map((item) => (
+            <CartItem key={item.id} product={item} />
+          ))}
+        </div>
         <Button>CHECKOUT</Button>
       </div>
     </div>
--- src/Components/Cart-Icon/cart-icon.component.jsx
+++ src/Components/Cart-Icon/cart-icon.component.jsx
@@ -1,12 +1,22 @@
+import { useContext } from 'react';
+
 import { ReactComponent as ShoppingIcon } from '../../assets/shopping-bag.svg';
+import { cartOpenContext } from '../../Contexts/CartOpen.context';
 
 import './cart-icon.styles.scss';
 
 const CartIcon = ({ onClick }) => {
+  const { cartItems } = useContext(cartOpenContext);
+
+  const itemCount = cartItems.reduce(
+    (accumulator, currentElement) => currentElement.quantity + accumulator,
+    0
+  );
+
   return (
     <div className="cart-icon-container" onClick={onClick}>
       <ShoppingIcon className="shopping-icon" />
-      <span className="item-count">0</span>
+      <span className="item-count">{itemCount}</span>
     </div>
   );
 };
