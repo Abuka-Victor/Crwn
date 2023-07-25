50d8a48db97410af1e8b2fcc76b47c9520607245
Abuka-Victor <abukavictoro@gmail.com>
2023-07-25 01:02:29 +0100

    Add payment gateway with Paystack

--- src/Routes/Checkout/Checkout.component.js
+++ src/Routes/Checkout/Checkout.component.js
@@ -2,18 +2,53 @@ import { useContext } from 'react';
 import CheckoutItem from '../../Components/Checkout-Item/Checkout-Item.component';
 import { cartContext } from '../../Contexts/Cart.context';
 
+import { PaystackButton } from 'react-paystack';
+
 import './checkout.styles.scss';
 import { useSelector } from 'react-redux';
 import {
   selectCartItems,
   selectTotal,
 } from '../../Store/reducers/cart/cart.selector';
+import Button, {
+  BUTTON_CLASSES,
+} from '../../Components/Button/Button.component';
+import { selectCurrentUser } from '../../Store/reducers/user/user.selector';
+
+const publish_key = process.env.REACT_APP_TEST_PUBLISH;
+const secret_key = process.env.REACT_APP_TEST_SECRET;
 
 const Checkout = () => {
   // const { cartItems, total } = useContext(cartContext);
 
   const cartItems = useSelector(selectCartItems);
   const total = useSelector(selectTotal);
+  const user = useSelector(selectCurrentUser);
+
+  console.log(JSON.stringify(user));
+
+  const onPaymentSuccess = (ref) => {
+    console.log(ref);
+  };
+
+  const onPaymentClosed = () => {
+    console.log('Payment Done');
+  };
+
+  const config = {
+    reference: new Date().getTime().toString(),
+    email: 'test@email.com',
+    amount: total * 100, //Amount is in the country's lowest currency. E.g Kobo, so 20000 kobo = N200
+    publicKey: publish_key,
+  };
+
+  const componentProps = {
+    ...config,
+    text: 'Pay',
+    onSuccess: (reference) => onPaymentSuccess(reference),
+    onClose: onPaymentClosed,
+  };
+
   return (
     <div className="checkout-container">
       <div className="checkout-header">
@@ -37,6 +72,7 @@ const Checkout = () => {
         <CheckoutItem key={item.id} product={item} />
       ))}
       <span className="total">Total: {`$${total}`}</span>
+      <PaystackButton {...componentProps} />
     </div>
   );
 };
